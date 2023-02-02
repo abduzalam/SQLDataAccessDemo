39f406d76018379febf84bc8270b8b45285f1c67
Abdul Kuzhiyil <Abdul.Kuzhiyil@ii-vi.com>
2023-02-02 11:40:29 +0000

    some changes

--- FormUI/Dashboard.cs
+++ FormUI/Dashboard.cs
@@ -4,8 +4,11 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Reflection;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
 using System.Windows.Forms;
 
 namespace FormUI
@@ -21,8 +24,29 @@ namespace FormUI
 
         private void Searchbutton_Click(object sender, EventArgs e)
         {
+
+            string methodName = "HelloMsg";
+            var typeString = "FormUI.DataAccess";
+            Type t = Type.GetType(typeString);
+            MethodInfo mi = t.GetMethod(methodName);
+
+            DataAccess daccess = new DataAccess();
+            string results = "";
+            if (mi != null)
+            {
+                results = (string)mi.Invoke(daccess, null);
+            }
+
+
             DataAccess db = new DataAccess();
+
+            db.TestSQL_IN_Clause();
             people = db.GetPeople(lastNametextBox.Text);
+            Person person = new Person();
+            person.FirstName = results;
+            person.LastName = "Reflection Test";
+            person.EmailAddress = "test@test.com";
+            people.Add(person);
             UpdateBinding();
         }
 
--- FormUI/DataAccess.cs
+++ FormUI/DataAccess.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,6 +29,63 @@ namespace FormUI
                 people.Add(new Person { FirstName = firstName, LastName = lastName, EmailAddress = emailAddress, PhoneNumber = phoneNumber });
                 connection.Execute("dbo.People_Insert @FirstName, @LastName, @EmailAddress, @PhoneNumber", people);
             }
+        }
+
+        public string HelloMsg()
+        {
+            return "Hello Using Reflection";
+        }
+
+        public void TestSQL()
+        {
+            string connectionString = Helper.CnnVal("SampleDB");
+            using(SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using(SqlCommand command = new SqlCommand("SELECT TOP 2 * FROM People",con))
+                {
+                    using(SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Console.WriteLine("{0} {1} {2}", reader.GetValue(0).ToString(), reader.GetString(1), reader.GetString(2));
+                        }
+                    }
+                }
+            }
+
+        }
+
+        public void TestSQL_IN_Clause()
+        {
+            string[] ids = new string[] { "1", "2", "3" };
+            string commandText = "SELECT * FROM People WHERE id in ({0})";
+            string[] paramNames = ids.Select(
+                (s,i)=>"@id"+i.ToString()).ToArray();
+            string inClause = string.Join(", ",paramNames);
+
+            string connectionString = Helper.CnnVal("SampleDB");
+            using (SqlConnection con = new SqlConnection(connectionString))
+            {
+                con.Open();
+                using (SqlCommand command = new SqlCommand(string.Format(commandText, inClause)))
+               
+                {
+                   for(int i = 0; i < paramNames.Length; i++)
+                    {
+                        command.Parameters.AddWithValue(paramNames[i], ids[i]);
+                    }
+                   command.Connection = con;
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine("{0} {1} {2}", reader.GetValue(0).ToString(), reader.GetString(1), reader.GetString(2));
+                        }
+                    }
+                }
+            }
+
         }
     }
 }
